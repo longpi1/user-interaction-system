63ecfc3ea05cf134d493ce5729309b415a747902
longpi1 <longping1999@163.com>
2024-01-26 23:06:55 +0800

    添加评论相关数据库表结构代码

---
+++ model/dao/db/model/comment_index.go
@@ -0,0 +1,35 @@
+package model
+
+import (
+	"gorm.io/gorm"
+)
+
+type Comment struct {
+	gorm.Model
+
+	Content string
+
+	PageKey  string `gorm:"index;size:255"`
+	SiteName string `gorm:"index;size:255"`
+
+	UserID uint `gorm:"index"`
+	UA     string
+	IP     string
+
+	Pid uint `gorm:"index"` // 父评论 ID
+
+	IsCollapsed bool `gorm:"default:false"` // 折叠
+	IsPending   bool `gorm:"default:false"` // 待审
+	IsPinned    bool `gorm:"default:false"` // 置顶
+
+	VoteUp   int
+	VoteDown int
+}
+
+func (c Comment) IsEmpty() bool {
+	return c.ID == 0
+}
+
+func (c Comment) IsAllowReply() bool {
+	return !c.IsCollapsed && !c.IsPending
+}
